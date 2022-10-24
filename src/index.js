ce8611dddbd7256b2ff8c49eff98feaf4d97e7e5
Tzvi H <16guitar@gmail.com>
2022-10-24 21:27:03 +0000

    Update 3 files

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import configureStore from './store';
+import {Provider} from 'react-redux'
+
+const store = configureStore()
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
